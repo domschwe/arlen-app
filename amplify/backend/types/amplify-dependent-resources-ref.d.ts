18bdfd8ff655f9f76ac4f30f23584de95161b468
Dom Schweyer <dom.schwe@gmail.com>
2022-02-05 18:29:17 -0500

    initial create

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "arlenapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
